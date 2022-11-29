c9f7f100b4c131c5a86ec92422c65e0b437fb948
mxrtydluffy <marty.marinda@students.dominican.edu>
2022-11-28 17:08:28 -0800

    added class

---
+++ product.py
@@ -0,0 +1,8 @@
+class Product:
+    def __init__(self, name, price):
+        self.name = name
+        self.price = price
+
+
+# product name, price
+
---
+++ store.py
@@ -0,0 +1,5 @@
+from department import Department
+
+class Store:
+    def __init__(self):
+        pass
