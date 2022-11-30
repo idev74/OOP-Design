47bfbc555f278546d3612f91fa03c46106e6f500
Isabella Devassy <77900249+idev74@users.noreply.github.com>
2022-11-30 15:29:00 -0800

    progress

--- store.py
+++ store.py
@@ -3,5 +3,6 @@
 from department import Department
 
 class Store:
-    def __init__(self):
-        pass
+    def __init__(self, name, total_profits):
+        self.name = name;
+        self.total_profits = total_profits;
