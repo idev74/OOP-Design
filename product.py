abf0d366fea2ba864b9f6993659486ebdb0702ca
mxrtydluffy <marty.marinda@students.dominican.edu>
2022-11-30 16:52:24 -0800

    finished product.py

--- product.py
+++ product.py
@@ -1,10 +1,20 @@
 # Marty
 
-class Product:
-    def __init__(self, name, price):
+class Store:
+    def __init__(self, name, sq_ft):
         self.name = name
-        self.price = price
+        self.sq_ft = sq_ft
+        self.max_capacity = round(self.sq_ft / 36) 
+
+    # Department has products
+    def get_name(self):
+        print('Please enter your store name: ')
+        return self.name
+        
+    def get_capacity(self): 
+        return f"{self.name}'s max capacity is {self.max_capacity}"
 
 
-# product name, price
-
+store = Store('Target', 50000)
+print(store.get_name())
+print(store.get_capacity())
