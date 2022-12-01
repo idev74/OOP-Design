04afc77a217419afccd84461339c4d6eef0e6381
Isabella Devassy <77900249+idev74@users.noreply.github.com>
2022-11-30 17:26:21 -0800

    Co-authored-by: Marty Marinda <mxrtydluffy@users.noreply.github.com>

--- department.py
+++ department.py
@@ -1,6 +1,15 @@
 # Isabella
+from store import Store
 
 class Department:
     def __init__(self, name, item_quantity):
-        self.name = name
-        self.item_amount = item_quantity
+        super().__init__(name)
+        self.item_quantity = item_quantity
+
+    def names(self):
+        pass
+
+    def get_quantity(self):
+        pass
+
+    
