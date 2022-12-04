07d42fd9596b856c2ed89f3ba5dfedf3bd2bd66a
Isabella Devassy <77900249+idev74@users.noreply.github.com>
2022-12-03 16:00:32 -0800

    added to classes

--- department.py
+++ department.py
@@ -1,15 +1,29 @@
 # Isabella
 from store import Store
+from product import Product, new
 
 class Department:
+    prod = new.products
+
+    dept_names = {
+            'clothing': prod,
+            'shoes': prod, 
+            'baby items': prod,
+            'groceries': prod,
+            'electronics': prod, 
+            'toys': prod,
+            'customer service': None
+    }
+
     def __init__(self, name, item_quantity):
-        super().__init__(name)
+        super(Store).__init__(name)
         self.item_quantity = item_quantity
 
-    def names(self):
-        pass
+    def selectName(self):
+        selection = input('Choose a department')
 
     def get_quantity(self):
         pass
 
-    
+for dept in Department.dept_names:
+    print(dept)
