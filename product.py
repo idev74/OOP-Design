3602031b7f12152c16a214a7cee637819dc8eb13
mxrtydluffy <marty.marinda@students.dominican.edu>
2022-12-03 16:00:01 -0800

    did product

--- product.py
+++ product.py
@@ -4,3 +4,11 @@ class Product:
     def __init__(self, name, price):
         self.name = name
         self.price = price
+        self.products = []
+
+    def _insert_obj(self):
+        self.products.append(self.name)
+        return self.products
+
+new = Product('toy', 4)
+print(new._insert_obj())
